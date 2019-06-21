376702df590339395f045dca9a222fbc7b3cf7cc
Nat Young <nat.young7@gmail.com>
2019-06-21 21:41:24 +0800

    | Nat, Aashish | Add CI config.

--- src/feature1.test.js
+++ src/feature1.test.js
@@ -1,7 +1,7 @@
 const feature1 = require("./feature1");
 
-describe('Blah blu', () => {
-  it('should eat a banana', () => {
+describe('Feature 1', () => {
+  it('should register a name', () => {
     expect(feature1.registerName({})).toEqual("Isha");
   });
 });
