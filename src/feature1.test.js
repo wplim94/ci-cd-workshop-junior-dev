c274c589ae137347dcacc6d3137df1e83edd1f01
Nat Young <nat.young7@gmail.com>
2019-06-21 23:02:13 +0800

    Fix failing test

--- src/feature1.test.js
+++ src/feature1.test.js
@@ -2,6 +2,6 @@ const feature1 = require("./feature1");
 
 describe('Feature 1', () => {
   it('should register a name', () => {
-    expect(feature1.registerName({})).toEqual("Isha");
+    expect(feature1.registerName({})).toEqual("Nat");
   });
 });
