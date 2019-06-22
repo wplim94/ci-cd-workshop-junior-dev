5beb0d109e2128d7711089e95af79b787ca12c1c
Lim Wan Ping <wp@bondlinc.com>
2019-06-22 11:45:46 +0800

    fix index.js auto-fixed by eslint

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ const feature1 = require('./feature1');
 
 const app = express();
 
-const server = app.listen(3000, () =>
-  console.log('Example app listening on port 3000!')
+app.listen(3000, () =>
+    console.log('Example app listening on port 3000!'),
 );
 
 app.get('/', (req, res) => {
   res.send(`Hello ${feature1.registerName(req)}`);
 });
 
-const stop = () => {
-  server.close();
-};
-
-module.exports = server;
-module.exports.stop = stop;
+app.get('/feature/version', (req, res) => {
+    const featureVersion = process.env.FEATURE_VERSION;
+    console.log(featureVersion);
+    res.send(!!featureVersion ? featureVersion : 'UNDEFINED');
+});
